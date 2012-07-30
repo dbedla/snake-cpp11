61fc309ff6c683b35b3be9adb55abc8ebe515c38
Dawid <dawid.bedla@gmail.com>
2012-07-30 19:09:06 +0200

    added constructors and some of methods to CItemsToEat

--- include/CItemsToEat.h
+++ include/CItemsToEat.h
@@ -3,17 +3,21 @@
 
 #include "CPoint.h"
 #include "IFrameElement.h"
+#include <algorithm>
 
 class CItemToEat:  public IFrameElement<PointsList >
 {
 public:
     CItemToEat();
+    CItemToEat(const CPoint&);
+    CItemToEat(const PointsList&);
     virtual const PointsList& getFrameElements() const;
-
+    void addElementToEat(const CPoint&);
+    void removeElementToEat(const CPoint&);
 private:
     //const unsigned int _numberOfItems;
     PointsList _itemsToEat;
-
+    void removeNormalizeElementsToEat();
 };
 
 #endif // CITEMSTOEAT_H
--- src/CItemToEat.cpp
+++ src/CItemToEat.cpp
@@ -3,7 +3,35 @@
 
 CItemToEat::CItemToEat(){}
 
+CItemToEat::CItemToEat(const CPoint& newPoint)
+{
+    _itemsToEat.push_back(newPoint);
+}
 
+
+CItemToEat::CItemToEat(const PointsList& srcPoints)
+{
+    std::for_each(srcPoints.begin(), srcPoints.end(),
+                  [&_itemsToEat](const CPoint& newPoint){_itemsToEat.push_back(newPoint);});
+}
+
+void CItemToEat::addElementToEat(const CPoint & newPoint)
+{
+    _itemsToEat.push_back(newPoint);
+    removeNormalizeElementsToEat();
+}
+
+void CItemToEat::removeElementToEat(const CPoint & pointToRemove)
+{
+    _itemsToEat.remove(pointToRemove);
+    removeNormalizeElementsToEat();
+}
+
+void CItemToEat::removeNormalizeElementsToEat()
+{
+    _itemsToEat.sort();
+    _itemsToEat.unique();
+}
 
 const PointsList& CItemToEat::getFrameElements() const
 {
